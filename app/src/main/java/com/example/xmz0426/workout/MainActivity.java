1d308a352c1ce6ae602ab77a37d6a148296a1a05
Isai <isai.ds18@gmail.com>
2016-04-26 17:39:25 -0500

    Adding Fragment Transaction

--- app/src/main/java/com/example/xmz0426/workout/MainActivity.java
+++ app/src/main/java/com/example/xmz0426/workout/MainActivity.java
@@ -1,11 +1,12 @@
 package com.example.xmz0426.workout;
 
 import android.os.Bundle;
+import android.app.FragmentTransaction;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends ActionBarActivity implements WorkoutListFragment.WorkoutListListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,5 +34,17 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void itemClicked(long id) {
+        WorkoutDetailFragment details = new WorkoutDetailFragment();
+        FragmentTransaction ft = getFragmentManager().beginTransaction();
+        details.setWorkout(id);
+        ft.replace(R.id.fragment_container,details);
+        ft.addToBackStack(null);
+        ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+        ft.commit();
+
     }
 }
